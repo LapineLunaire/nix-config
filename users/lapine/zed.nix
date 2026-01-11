51cada3a4cda1b14d9d1eb36f49c2a984827966a
Lapine <lapine@lunaire.eu>
2026-01-12 00:19:14 +0100

    Add nixd LSP support for Zed

--- users/lapine/zed.nix
+++ users/lapine/zed.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: {
   programs.zed-editor = {
     enable = true;
     package = pkgs.zed-editor.overrideAttrs (oldAttrs: {doCheck = false;});
@@ -7,9 +11,14 @@
       load_direnv = "shell_hook";
       vim_mode = true;
       hour_format = "hour24";
+      lsp.nixd.settings.nixpkgs.expr = "import <nixpkgs> {}";
     };
     extensions = [
       "nix"
     ];
   };
+
+  home.packages = [
+    inputs.nixd.packages.${pkgs.system}.nixd
+  ];
 }
