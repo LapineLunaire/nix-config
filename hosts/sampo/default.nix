1c2456c7562e33a011cac33991adcd2853a8265e
Lapine <lapine@lunaire.eu>
2026-01-11 11:20:32 +0100

    Fix sshd configuration

--- hosts/sampo/default.nix
+++ hosts/sampo/default.nix
@@ -26,10 +26,13 @@
       "/var/lib/systemd"
       "/var/lib/NetworkManager"
       "/var/log"
-      "/etc/ssh"
     ];
     files = [
       "/etc/machine-id"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
     ];
   };
 
