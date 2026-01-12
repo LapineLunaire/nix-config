3adde2406d5ea756250534c003d243ade0580e8b
Lapine <lapine@lunaire.eu>
2026-01-12 22:16:50 +0100

    Allow wheel group to reboot/shutdown without password

--- hosts/generic/security.nix
+++ hosts/generic/security.nix
@@ -1,7 +1,24 @@
-{...}: {
+{pkgs, ...}: {
   # "!" disables root login
   users.users.root.hashedPassword = "!";
   users.mutableUsers = false;
 
-  security.polkit.enable = true;
+  security.polkit = {
+    enable = true;
+    extraConfig = ''
+      polkit.addRule(function (action, subject) {
+        if (
+          subject.isInGroup("wheel") &&
+          [
+            "org.freedesktop.login1.reboot",
+            "org.freedesktop.login1.reboot-multiple-sessions",
+            "org.freedesktop.login1.power-off",
+            "org.freedesktop.login1.power-off-multiple-sessions",
+          ].indexOf(action.id) !== -1
+        ) {
+          return polkit.Result.YES;
+        }
+      });
+    '';
+  };
 }
