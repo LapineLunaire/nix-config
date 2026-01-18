8cb8c7bde3f15c4d3ac350365996140dfb2a62ae
Lapine <lapine@lunaire.eu>
2026-01-18 22:41:52 +0100

    Configure ALSA settings for FiiO K11 to reduce crackling

--- hosts/generic/services.nix
+++ hosts/generic/services.nix
@@ -41,6 +41,25 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+
+    wireplumber.extraConfig = {
+      "51-fiio-k11" = {
+        "monitor.alsa.rules" = [
+          {
+            matches = [
+              { "device.name" = "alsa_card.usb-FIIO_FiiO_K11-01"; }
+            ];
+            actions = {
+              "update-props" = {
+                "api.alsa.period-size" = 256;
+                "api.alsa.headroom" = 0;
+                "audio.format" = "S32LE";
+              };
+            };
+          }
+        ];
+      };
+    };
   };
 
   services.openssh = {
