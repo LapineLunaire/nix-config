a6c71af347729ac53d90dc54a40f09878515e252
Lapine <lapine@lunaire.eu>
2026-01-19 21:47:25 +0100

    Configure ZFS unstable package for sparkle

--- hosts/sparkle/default.nix
+++ hosts/sparkle/default.nix
@@ -11,7 +11,10 @@
     networkmanager.enable = true;
   };
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    zfs.package = pkgs.zfs_unstable;
+  };
 
   system.stateVersion = "25.11";
 }
