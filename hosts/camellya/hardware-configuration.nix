2aec6d3d2f242a9e35d00b261faa33f23b5b9429
Lapine <lapine@lunaire.eu>
2026-01-24 13:04:04 +0100

    Add torrents Samba mount on camellya

--- hosts/camellya/hardware-configuration.nix
+++ hosts/camellya/hardware-configuration.nix
@@ -54,6 +54,21 @@
     ];
   };
 
+  fileSystems."/home/lapine/torrents" = {
+    device = "//10.28.32.25/torrents";
+    fsType = "cifs";
+    options = [
+      "credentials=/etc/samba-credentials"
+      "uid=1000"
+      "gid=100"
+      "x-systemd.automount"
+      "noauto"
+      "x-systemd.idle-timeout=60"
+      "x-systemd.device-timeout=5s"
+      "x-systemd.mount-timeout=5s"
+    ];
+  };
+
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/9864-73D6";
     fsType = "vfat";
