28491d4739bce02374c43088d0f2aceb62f0f16e
Lapine <lapine@lunaire.eu>
2026-01-31 08:46:26 +0100

    Update flake.lock

--- flake.nix
+++ flake.nix
@@ -12,6 +12,7 @@
     impermanence = {
       url = "github:nix-community/impermanence";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
     };
 
     nixd = {
