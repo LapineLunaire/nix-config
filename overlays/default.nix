bfae7f1fa0d8c86c56b712d0b2fa1b67c75099a7
Lapine <lapine@lunaire.eu>
2026-02-02 00:25:44 +0100

    Simplify ffmpeg overrides to only mpv and ffmpeg-full

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,14 +4,13 @@
 
   # This overlay can be used to modify or add custom versions of packages
   modifications = final: prev: {
-    ffmpeg = prev.ffmpeg.override {withUnfree = true;};
     ffmpeg-full = prev.ffmpeg-full.override {withUnfree = true;};
-    ffmpeg_6 = prev.ffmpeg_6.override {withUnfree = true;};
-    ffmpeg_6-full = prev.ffmpeg_6-full.override {withUnfree = true;};
-    ffmpeg_7 = prev.ffmpeg_7.override {withUnfree = true;};
-    ffmpeg_7-full = prev.ffmpeg_7-full.override {withUnfree = true;};
-    ffmpeg_8 = prev.ffmpeg_8.override {withUnfree = true;};
-    ffmpeg_8-full = prev.ffmpeg_8-full.override {withUnfree = true;};
+
+    mpv = prev.mpv.override {
+      mpv-unwrapped = prev.mpv-unwrapped.override {
+        ffmpeg = prev.ffmpeg.override {withUnfree = true;};
+      };
+    };
 
     protonmail-desktop = prev.protonmail-desktop.overrideAttrs (oldAttrs: {
       postFixup =
