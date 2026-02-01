fa286f43c39c8e8e3b886f94d0b8aaf1816154c8
Lapine <lapine@lunaire.eu>
2026-02-01 13:49:31 +0100

    Fix protonmail-desktop by forcing X11 mode

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,9 +3,12 @@
   additions = final: _prev: import ../pkgs final.pkgs;
 
   # This overlay can be used to modify or add custom versions of packages
-  modifications = _final: _prev: {
-    # example = prev.example.overrideAttrs (oldAttrs: rec {
-    # ...
-    # });
+  modifications = final: prev: {
+    protonmail-desktop = prev.protonmail-desktop.overrideAttrs (oldAttrs: {
+      postFixup = (oldAttrs.postFixup or "") + ''
+        wrapProgram $out/bin/proton-mail \
+          --add-flags "--enable-features=UseOzonePlatform --ozone-platform=x11"
+      '';
+    });
   };
 }
