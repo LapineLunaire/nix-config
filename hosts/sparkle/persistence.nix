95ca40f46d06349203db1b16fcfde4495de5ffef
Lapine <lapine@lunaire.eu>
2026-02-01 14:00:35 +0100

    Remove RSA SSH host key support, use only ed25519

--- hosts/sparkle/persistence.nix
+++ hosts/sparkle/persistence.nix
@@ -18,8 +18,6 @@
       "/etc/machine-id"
       "/etc/ssh/ssh_host_ed25519_key"
       "/etc/ssh/ssh_host_ed25519_key.pub"
-      "/etc/ssh/ssh_host_rsa_key"
-      "/etc/ssh/ssh_host_rsa_key.pub"
     ];
   };
 
--- modules/nixos/generic/services.nix
+++ modules/nixos/generic/services.nix
@@ -21,5 +21,11 @@
       PermitRootLogin = "no";
       PasswordAuthentication = false;
     };
+    hostKeys = [
+      {
+        path = "/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+    ];
   };
 }
