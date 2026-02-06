e99e21affd8aef4a3c3f479368b040b5ef623602
Lapine <lapine@lunaire.eu>
2026-02-06 20:29:18 +0100

    Add admin token to vaultwarden

--- hosts/sparkle/services/vaultwarden.nix
+++ hosts/sparkle/services/vaultwarden.nix
@@ -8,6 +8,8 @@
       ROCKET_PORT = 6000;
       SIGNUPS_ALLOWED = false;
       DATABASE_URL = "postgresql://vaultwarden@/vaultwarden";
+      # Generate with: vaultwarden hash
+      ADMIN_TOKEN = "$argon2id$v=19$m=65540,t=3,p=4$JptV30FnYD5j6JifJnQvAi6L788zr6ZSY0XY2/X9miU$HMFFns8fg7aEx2fcZ/vNl79gaXQq9a8OAgRcV8v3X+U";
     };
   };
 }
