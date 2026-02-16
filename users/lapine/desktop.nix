55814cb0983eca6145d9293e36aef95b05e370e4
Lapine <lapine@lunaire.eu>
2026-02-16 18:20:39 +0100

    Add XDG desktop portal configuration for Hyprland

--- users/lapine/desktop.nix
+++ users/lapine/desktop.nix
@@ -276,6 +276,13 @@ in {
 
     xdg = {
       enable = true;
+
+      portal = {
+        enable = true;
+        extraPortals = [pkgs.xdg-desktop-portal-hyprland pkgs.xdg-desktop-portal-gtk];
+        config.common.default = ["hyprland" "gtk"];
+      };
+
       userDirs = {
         enable = true;
         createDirectories = true;
