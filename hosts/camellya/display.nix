d0cb1d9ebc89f326db7caf8a4a9bfbe008564d90
Lapine <lapine@lunaire.eu>
2026-03-30 09:35:26 +0200

    camellya: fix windowrule clobbering desktop rules

--- hosts/camellya/display.nix
+++ hosts/camellya/display.nix
@@ -1,9 +1,9 @@
-{...}: {
+{lib, ...}: {
   home-manager.users.carmilla = {
     wayland.windowManager.hyprland.settings = {
       monitor = ["DP-2, 2560x1440@165.08, 0x0, 1"];
       general.allow_tearing = true;
-      windowrule = ["immediate on, match:class .*"];
+      windowrule = lib.mkAfter ["immediate on, match:class .*"];
     };
   };
 }
