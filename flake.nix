2cc7d07c811a4fa8c4e53a47dff1004e6557fe6d
Lapine <lapine@lunaire.eu>
2026-04-07 12:59:51 +0200

    flake: switch nixpkgs to nixos-unstable-small

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Carmilla's nix config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
 
     aagl = {
       url = "github:ezKEa/aagl-gtk-on-nix";
