ee0fc12d090812170f7c1e58014e2963c778508d
Lapine <lapine@lunaire.eu>
2026-04-10 09:30:10 +0200

    packages: add texlive full to darwin and desktop

--- users/carmilla/packages.nix
+++ users/carmilla/packages.nix
@@ -26,6 +26,7 @@
     ++ lib.optionals (config.userConfig.desktop.enable || pkgs.stdenv.hostPlatform.isDarwin) (with pkgs; [
       alejandra
       azahar
+      texlive.combined.scheme-full
       bat
       discord
       duf
