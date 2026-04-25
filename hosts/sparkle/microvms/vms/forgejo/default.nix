5e1e01a66abb9ba9888080af7be8b875b5875a7e
Lapine <lapine@lunaire.eu>
2026-04-25 07:36:47 +0200

    sparkle: start postgres VM before its dependents

--- hosts/sparkle/microvms/vms/forgejo/default.nix
+++ hosts/sparkle/microvms/vms/forgejo/default.nix
@@ -4,6 +4,11 @@
     evaluatedConfig = outputs.nixosConfigurations.forgejo;
   };
 
+  systemd.services."microvm@forgejo" = {
+    after = ["microvm@postgres.service"];
+    wants = ["microvm@postgres.service"];
+  };
+
   systemd.network.networks."10-forgejo" = {
     matchConfig.Name = "forgejo";
     networkConfig.Bridge = "vm-br0";
