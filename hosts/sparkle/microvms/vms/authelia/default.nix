a522f70d5142a8ad09f71371b60014c7f3469d39
Lapine <lapine@lunaire.eu>
2026-04-25 07:36:47 +0200

    sparkle: start postgres VM before its dependents

--- hosts/sparkle/microvms/vms/authelia/default.nix
+++ hosts/sparkle/microvms/vms/authelia/default.nix
@@ -4,6 +4,11 @@
     evaluatedConfig = outputs.nixosConfigurations.authelia;
   };
 
+  systemd.services."microvm@authelia" = {
+    after = ["microvm@postgres.service"];
+    wants = ["microvm@postgres.service"];
+  };
+
   systemd.network.networks."10-authelia" = {
     matchConfig.Name = "authelia";
     networkConfig.Bridge = "vm-br0";
--- hosts/sparkle/microvms/vms/vaultwarden/default.nix
+++ hosts/sparkle/microvms/vms/vaultwarden/default.nix
@@ -4,6 +4,11 @@
     evaluatedConfig = outputs.nixosConfigurations.vaultwarden;
   };
 
+  systemd.services."microvm@vaultwarden" = {
+    after = ["microvm@postgres.service"];
+    wants = ["microvm@postgres.service"];
+  };
+
   systemd.network.networks."10-vaultwarden" = {
     matchConfig.Name = "vaultwarden";
     networkConfig.Bridge = "vm-br0";
