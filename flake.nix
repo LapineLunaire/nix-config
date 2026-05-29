3ec6681a20335dcb70ea97ed1397a0c8012e2e15
Lapine <lapine@lunaire.eu>
2026-05-29 19:59:41 +0200

    flake: track nixpkgs-unstable instead of nixos-unstable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-26.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager/master";
