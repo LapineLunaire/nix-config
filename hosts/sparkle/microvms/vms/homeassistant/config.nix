6246482b1913793d433593a8fe62d2dbbd27e458
Lapine <lapine@lunaire.eu>
2026-06-15 02:00:52 +0000

    sparkle: update container image digests 2026-06-15

--- hosts/sparkle/microvms/vms/homeassistant/config.nix
+++ hosts/sparkle/microvms/vms/homeassistant/config.nix
@@ -45,7 +45,7 @@
   ];
 
   virtualisation.oci-containers.containers.homeassistant = {
-    image = "ghcr.io/home-assistant/home-assistant@sha256:59aa8824955c9db491b75d2eebe42bd68494f80c2ec69ec0d66d9dae37d37514";
+    image = "ghcr.io/home-assistant/home-assistant@sha256:aed891b8f801072302815b4b0fab5adb714182967e9d2e2d4a2be558241c73ad";
     autoStart = true;
     volumes = ["/persist/var/lib/hass:/config"];
     environment.TZ = "Etc/UTC";
