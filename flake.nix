10ff3118a41758bfcdf9be9c7dd6b916279362fc
Lapine <lapine@lunaire.eu>
2026-06-21 14:59:05 +0200

    flake: track the tested nixos-26.05 and nixos-unstable channels

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Carmilla's nix config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-26.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
