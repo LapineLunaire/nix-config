0401f79f557182c7131794ccb03c434c0997feef
Lapine <lapine@lunaire.eu>
2026-07-04 20:15:21 +0200

    users: share the yubikey ssh public keys with the microvm guests

--- modules/nixos/microvm-guest.nix
+++ modules/nixos/microvm-guest.nix
@@ -65,10 +65,7 @@ in {
         }
       ];
     };
-    users.users.root.openssh.authorizedKeys.keys = [
-      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIEes6fnuE4zIKuneekCyPzMYItOOgfnDo0Eiakvwf62mAAAACnNzaDpsYXBpbmU="
-      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIMqXDPM9z04YBOp2fVDox7sgPFNpad+9p8UA+od8V8nxAAAACnNzaDpsYXBpbmU="
-    ];
+    users.users.root.openssh.authorizedKeys.keys = import ../../users/carmilla/ssh-keys.nix;
     networking.firewall.extraInputRules = lib.mkMerge [
       (lib.mkBefore ''
         ip saddr ${net.ip.monitoring} tcp dport 9100 accept
--- users/carmilla/default.nix
+++ users/carmilla/default.nix
@@ -22,11 +22,7 @@
         ["wheel"]
         ++ lib.optionals config.networking.networkmanager.enable ["networkmanager"]
         ++ lib.optionals config.home-manager.users.carmilla.userConfig.desktop.enable ["video" "audio" "input"];
-      # Two FIDO2 resident keys (sk-ssh-ed25519) from separate YubiKeys for redundancy.
-      openssh.authorizedKeys.keys = [
-        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIEes6fnuE4zIKuneekCyPzMYItOOgfnDo0Eiakvwf62mAAAACnNzaDpsYXBpbmU="
-        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIMqXDPM9z04YBOp2fVDox7sgPFNpad+9p8UA+od8V8nxAAAACnNzaDpsYXBpbmU="
-      ];
+      openssh.authorizedKeys.keys = import ./ssh-keys.nix;
     };
 
   home-manager.backupFileExtension = "bak";
