1855ec16bf56a800bd4a6966198c46f07058374a
Lapine <lapine@lunaire.eu>
2026-07-04 08:27:42 +0200

    camellya: persist networkmanager connections and state

--- hosts/camellya/persistence.nix
+++ hosts/camellya/persistence.nix
@@ -2,6 +2,8 @@
   environment.persistence."/persist" = {
     hideMounts = true;
     directories = [
+      "/etc/NetworkManager/system-connections"
+      "/var/lib/NetworkManager"
       "/var/lib/nixos"
       "/var/lib/sbctl"
       "/var/lib/systemd"
