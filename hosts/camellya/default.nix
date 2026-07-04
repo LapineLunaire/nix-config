3933d704d6ce4439162feaac1228f62864e5ee8e
Lapine <lapine@lunaire.eu>
2026-07-04 16:07:13 +0200

    camellya: drop the autosuspend udev rule for usb mic

--- hosts/camellya/default.nix
+++ hosts/camellya/default.nix
@@ -42,10 +42,6 @@
     wooting-udev-rules
   ];
 
-  services.udev.extraRules = ''
-    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="046d", ATTR{idProduct}=="0ab7", ATTR{power/autosuspend}="-1"
-  '';
-
   services.xserver.videoDrivers = ["nvidia"];
 
   system.stateVersion = "26.11";
